6ecde74211f564db13094830ca1fee705588b3b0
DupinderSingh <dupinder.15c@gmail.com>
2019-11-28 15:39:53 +0530

    made changes

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {storiesOf} from '@storybook/react';
+
 import 'tui-image-editor/dist/tui-image-editor.css';
 import 'tui-color-picker/dist/tui-color-picker.css';
 
 import ImageEditor from '../src/index';
-// import {storiesOf} from '@storybook/react';
 
-// const stories = storiesOf('Toast UI ImageEditor', module);
+const stories = storiesOf('Toast UI ImageEditor', module);
 
 const props = {
   includeUI: {
@@ -23,63 +24,104 @@ const props = {
   cssMaxHeight: 500
 };
 
-class Story extends React.Component {
-  ref = React.createRef();
+stories.add('Include default UI', () => <ImageEditor {...props} />);
 
-  imageEditor = null;
+stories.add('Using Method', () => {
+  class Story extends React.Component {
+    ref = React.createRef();
 
-  componentDidMount() {
-    this.imageEditor = this.ref.current.getInstance();
-    // get the data from api and set to the editor. // get all the "type" match variable and load one by one onto the editor.
-    window.setTimeout(() => {
-      // check for every object and according to it set the function..... eg addShape for square, rect. addImage for image.....
-      this.imageEditor.addShape('rect', {
-        id: 100,
-        fill: 'red',
-        stroke: 'blue',
-        strokeWidth: 3,
-        width: 100,
-        height: 200,
-        left: 10,
-        top: 10,
-        isRegular: true
+    imageEditor = null;
+
+    componentDidMount() {
+      this.imageEditor = this.ref.current.getInstance();
+      // get the data from api and set to the editor. // get all the "type" match variable and load one by one onto the editor.
+      window.setTimeout(() => {
+        // check for every object and according to it set the function..... eg addShape for square, rect. addImage for image.....
+        this.imageEditor.addShape('rect', {
+          id: 100,
+          fill: 'red',
+          stroke: 'blue',
+          strokeWidth: 3,
+          width: 100,
+          height: 200,
+          left: 10,
+          top: 10,
+          isRegular: true
+        });
+
+
+        // let getPosition = this.imageEditor.getObjectPosition(100, 'left', 'top');
+        // console.log(getPosition, "getPosition")
+      }, 2000);
+    }
+
+    getTriangle() {
+      // this.imageEditor.addShape('rect', {
+      //   fill: 'red',
+      //   stroke: 'blue',
+      //   strokeWidth: 3,
+      //   width: 100,
+      //   height: 200,
+      //   left: 10,
+      //   top: 10,
+      //   isRegular: true
+      // });
+    }
+
+    flipImageByAxis(isXAxis) {
+      this.imageEditor[isXAxis ? 'flipX' : 'flipY']()
+        .then((status) => {
+          console.log('flipX: ', status.flipX);
+          console.log('flipY: ', status.flipY);
+          console.log('angle: ', status.angle);
+        })
+        ['catch']((message) => {
+        console.log('error: ', message);
       });
+    }
 
-
-      // let getPosition = this.imageEditor.getObjectPosition(100, 'left', 'top');
-      // console.log(getPosition, "getPosition")
-    }, 2000);
+    render() {
+      return (
+        <>
+          <ImageEditor ref={this.ref} {...props} />
+          <button
+            onClick={this.getTriangle.bind(this)}>
+            get me rect
+          </button>
+          <button
+            onClick={() => {
+              this.flipImageByAxis(true);
+            }}
+          >
+            Flip-X!
+          </button>
+          <button
+            onClick={() => {
+              this.flipImageByAxis(false);
+            }}
+          >
+            Flip-Y!
+          </button>
+        </>
+      );
+    }
   }
 
-  getTriangle() {
-    // this.imageEditor.addShape('rect', {
-    //   fill: 'red',
-    //   stroke: 'blue',
-    //   strokeWidth: 3,
-    //   width: 100,
-    //   height: 200,
-    //   left: 10,
-    //   top: 10,
-    //   isRegular: true
-    // });
-  }
+  return <Story />;
+});
 
-  flipImageByAxis(isXAxis) {
-    this.imageEditor[isXAxis ? 'flipX' : 'flipY']()
-      .then((status) => {
-        console.log('flipX: ', status.flipX);
-        console.log('flipY: ', status.flipY);
-        console.log('angle: ', status.angle);
-      })
-      ['catch']((message) => {
-      console.log('error: ', message);
-    });
-  }
+stories.add('Events', () => {
+  class Story2 extends React.Component {
+    ref = React.createRef();
 
-  render() {
-    // on anything change or added update we have to update the json side by side.......
-    return (
-      <>
+    imageEditor = null;
+
+    componentDidMount() {
+      this.imageEditor = this.ref.current.getInstance();
+    }
+
+    render() {
+      return (
         <ImageEditor
           ref={this.ref}
           {...props}
@@ -108,25 +150,9 @@ class Story extends React.Component {
             console.log(props.id);
           }}
         />
-        <button
-          onClick={this.getTriangle.bind(this)}>
-          get me rect
-        </button>
-        <button
-          onClick={() => {
-            this.flipImageByAxis(true);
-          }}
-        >
-          Flip-X!
-        </button>
-        <button
-          onClick={() => {
-            this.flipImageByAxis(false);
-          }}
-        >
-          Flip-Y!
-        </button>
-      </>
-    );
+      );
+    }
   }
-}
+
+  return <Story2 />;
+});
